715881382a8a988bc02091ee5aa83ef551fdc062
AlexF <frolov1904@mail.ru>
2025-08-03 20:50:43 +0300

    First commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+def hw_5():
+    driver= webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
 
-driver= webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
-
-icon1= driver.find_element(By.CSS_SELECTOR,'#user-name')
-icon2= driver.find_element(By.CSS_SELECTOR,'#password')
-icon3= driver.find_element(By.CSS_SELECTOR,'#login-button')
-if icon1 and icon2 and icon3 is not(None):
-    print('Элементы найдены')
-else:
-    print('Элементы не найдены')
+    icon1= driver.find_element(By.CSS_SELECTOR,'#user-name')
+    icon2= driver.find_element(By.CSS_SELECTOR,'#password')
+    icon3= driver.find_element(By.CSS_SELECTOR,'#login-button')
+    if icon1 and icon2 and icon3 is not(None):
+        print('Элементы найдены')
+    else:
+        print('Элементы не найдены')
+hw_5()
